fc5932e71b12735d21e885ab3096583a208d8d91
Marat Shakirov <marat@shakirov.name>
2024-12-21 17:08:19 +0300

    Change to django

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '5.45.127.218'
+]
 
 
 # Application definition
