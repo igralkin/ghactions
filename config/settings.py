ca688889bd89ac141ea3cbcf58d3563f2e89a2cd
Marat Shakirov <marat@shakirov.name>
2024-12-21 16:57:17 +0300

    Change to django

--- config/settings.py
+++ config/settings.py
@@ -90,7 +90,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD')
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
